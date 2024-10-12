13774797efcfd4c3524aca48d1867c27b94cfb88
Muhammad Nur Hidayat <dayaters22@gmail.com>
2024-10-12 21:38:15 +0700

    [Fix] Transaction detail: fix not found validation

--- apps/services/transaction.service.js
+++ apps/services/transaction.service.js
@@ -5,7 +5,7 @@ const transactionService = {
 
   getByIdValidate: async (trxId) => {
     const getData = await transactionModel.selectByTrxId(trxId).then(res => res[0])
-    if (!getData.id) throw new ApiError(404, 'Transaction is not found')
+    if (!getData?.id) throw new ApiError(404, 'Transaction is not found')
     return getData
   }
 
